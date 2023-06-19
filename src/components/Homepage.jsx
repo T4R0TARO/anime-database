985d8150ba519fe4b2703c020244ca40d6b6dc46
josh manansala <joshmanansalagsd@gmail.com>
2023-06-19 16:52:08 -0700

    added logic to hompage button features

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
-import Popular from "./Popular";
-import { useGlobalContext } from "../context/Global";
+import Popular from "./Popular.jsx";
+import { useGlobalContext } from "../context/Global.jsx";
+import "../styles/Homepage.css";
 
 export default function Homepage() {
+  const {
+    handleChange,
+    search,
+    handleSubmit,
+    searchAnime,
+    getPopularAnime,
+    getUpcomingAnime,
+    getAiringAnime,
+  } = useGlobalContext();
+
   const [rendered, setRendered] = useState("popular");
+  // console.log(rendered);
 
   const switchComponents = () => {
     switch (rendered) {
@@ -16,7 +28,6 @@ export default function Homepage() {
 
   return (
     <div className="Homepage">
-      <h1>Homepage</h1>
       <header>
         <div className="logo">
           <h1>
@@ -26,15 +37,58 @@ export default function Homepage() {
               ? "Airing Anime"
               : "Upcoming Anime"}
           </h1>
-          {/* SEARCH BAR */}
-          <div className="search-container">
-            {/* popular button */}
-            {/* search form */}
-            {/* airing button */}
-            {/* upcoming button */}
+        </div>
+        {/* SEARCH BAR */}
+        <div className="search-container">
+          {/* popular button */}
+          <div className="filter-btn popular-filter">
+            <button
+              onClick={() => {
+                setRendered("popular");
+              }}
+            >
+              Popular
+            </button>
+          </div>
+          {/* search form */}
+          <form action="" className="search-form">
+            <div className="input-control">
+              <input
+                type="text"
+                placeholder="Search Anime"
+                value={search}
+                onChange={handleChange}
+              />
+              <button type="submit" onClick={handleSubmit}>
+                Search
+              </button>
+            </div>
+          </form>
+          {/* airing button */}
+          <div className="filter-btn airing-filter">
+            <button
+              onClick={() => {
+                setRendered("airing");
+                getAiringAnime();
+              }}
+            >
+              Airing
+            </button>
+          </div>
+          {/* upcoming button */}
+          <div className="filter-btn upcoming-filter">
+            <button
+              onClick={() => {
+                setRendered("upcoming");
+                getUpcomingAnime();
+              }}
+            >
+              Upcoming
+            </button>
           </div>
         </div>
       </header>
+      {/* Display Data Content */}
       {switchComponents()}
     </div>
   );
--- src/context/Global.jsx
+++ src/context/Global.jsx
@@ -1,4 +1,10 @@
-import React, { useReducer, createContext, useContext, useEffect } from "react";
+import React, {
+  useReducer,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 // Create a new context
 const GlobalContext = createContext();
@@ -19,6 +25,12 @@ const reducer = (state, action) => {
       return { ...state, loading: true };
     case GET_POPULAR_ANIME:
       return { ...state, popularAnime: action.payload, loading: false };
+    case GET_AIRING_ANIME:
+      return { ...state, airingAnime: action.payload, loading: false };
+    case GET_UPCOMING_ANIME:
+      return { ...state, upcomingAnime: action.payload, loading: false };
+    case SEARCH:
+      return { ...state, searchResults: action.payload, loading: false };
     default:
       return state;
   }
@@ -40,6 +52,29 @@ export const GlobalContextProvider = ({ children }) => {
   // useReducer
   const [state, dispatch] = useReducer(reducer, initalState);
 
+  // Form Input State
+  const [search, setSearch] = useState("");
+
+  // Form Input Onchange
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    if (e.target.value === "") {
+      state.isSearch = false;
+    }
+  };
+
+  // Form Search Button onClick
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (search) {
+      searchAnime(search);
+      state.isSearch = true;
+    } else {
+      state.isSearch = false;
+      alert("Please enter a search term");
+    }
+  };
+
   // fetch popular anime
   const getPopularAnime = async () => {
     dispatch({ type: LOADING });
@@ -47,6 +82,32 @@ export const GlobalContextProvider = ({ children }) => {
     const data = await response.json();
     dispatch({ type: GET_POPULAR_ANIME, payload: data.data });
     // console.log(data.data);
+  };
+
+  // airing anime
+  const getAiringAnime = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(`${baseUrl}/top/anime?filter=airing`);
+    const data = await response.json();
+    dispatch({ type: GET_AIRING_ANIME, payload: data.data });
+  };
+
+  // upcoming anime
+  const getUpcomingAnime = async () => {
+    dispatch({ type: LOADING });
+    const response = await fetch(`${baseUrl}/top/anime?filter=upcoming`);
+    const data = await response.json();
+    dispatch({ type: GET_UPCOMING_ANIME, payload: data.data });
+  };
+
+  // search anime
+  const searchAnime = async (anime) => {
+    dispatch({ type: LOADING });
+    const response = await fetch(
+      `https://api/jikan.moe/v4/anime?q=${anime}&order_by=popularity&sort=asc&sfw`
+    );
+    const data = await response.json();
+    dispatch({ type: SEARCH, payload: data.data });
   };
 
   // useEffect()
@@ -59,6 +120,13 @@ export const GlobalContextProvider = ({ children }) => {
     <GlobalContext.Provider
       value={{
         ...state,
+        handleChange,
+        search,
+        handleSubmit,
+        searchAnime,
+        getAiringAnime,
+        getUpcomingAnime,
+        getPopularAnime,
       }}
     >
       {children}
